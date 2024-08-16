c35ed04b494d5780db258b077a20326d202bfdcc
omarbramirez <omar.ramirez94@hotmail.es>
2024-08-16 08:00:59 -0600

    adding routing features and customized params

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { ProductComponent } from './product/product.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'products', component: ProductComponent},
+    {path: 'products/:productId', component: ProductDetailComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: 'profile', component: ProfileComponent},
+    {path: '**', redirectTo: '/home', pathMatch: 'full'}
+];
---
+++ src/app/product-detail/product-detail.component.ts
@@ -0,0 +1,22 @@
+import { Component,OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-product-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-detail.component.html',
+  styleUrl: './product-detail.component.css'
+})
+export class ProductDetailComponent implements OnInit {
+
+  producto: string = ''
+
+  constructor(private _route: ActivatedRoute) {}
+
+  ngOnInit():void{
+    this._route.params.subscribe(params => {
+      this.producto = params['productId']
+    })
+  }
+}
